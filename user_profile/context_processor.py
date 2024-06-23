f61d684b9f7ddc89d3d01f2aceb3f05c0aaaba54
Denes Kalnoky <dkalnoky@gmail.com>
2024-06-23 23:15:40 +0000

    added context processor for wishlist count

---
+++ user_profile/context_processor.py
@@ -0,0 +1,9 @@
+from products.models import Product
+
+
+def wishlist_count(request):
+    if request.user.is_authenticated:
+        count = Product.objects.filter(users_wishlist=request.user).count()
+    else: 
+        count = 0
+    return {'wishlist_count': count}
