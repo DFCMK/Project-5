b07b5253f962d8eae3182e37be733b9a4adcb505
Denes Kalnoky <dkalnoky@gmail.com>
2024-06-25 20:35:53 +0000

    deleted unused adresses form

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import UserProfile, Address
+from .models import UserProfile
 
 
 class UserProfileForm(forms.ModelForm):
@@ -36,16 +36,3 @@ class UserProfileForm(forms.ModelForm):
             self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
             self.fields[field].label = False
 
-class Addresses(forms.ModelForm):
-    class Meta:
-        model = Address
-        fields = {
-            'default_full_name': 'Full_name',
-            'default_phone_number': 'Phone Number',
-            'default_country': 'Country',
-            'default_postcode': 'Postal Code',
-            'default_town_or_city': 'Town or City',
-            'default_street_address1': 'Street Address 1',
-            'default_street_address2': 'Street Address 2',
-            'default_county': 'County',
-        }
