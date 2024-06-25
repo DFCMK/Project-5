a6ca455584c37d877cc265ff4bb4c3b68e31710e
Denes Kalnoky <dkalnoky@gmail.com>
2024-06-25 20:37:04 +0000

    added remove_from_wishlist view to urlspatterns

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
     path('profile/wishlist/', views.wishlist, name='wishlist'),
-    path('wishlist/add_to_wishlist/<int:id>', views.add_to_wishlist, name='add_to_wishlist')
+    path('wishlist/add_to_wishlist/<int:id>', views.add_to_wishlist, name='add_to_wishlist'),
+    path('wishlist/remove_from_wishlist/<int:id>/', views.remove_from_wishlist, name='remove_from_wishlist')
     ]
