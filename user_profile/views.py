1b7fb6bbf055f0683ae7f2e01da8dbeac7d214a1
Denes Kalnoky <dkalnoky@gmail.com>
2024-06-30 20:39:29 +0000

    created add_address view

--- user_profile/views.py
+++ user_profile/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .models import UserProfile, Wishlist
+from .models import UserProfile, Wishlist, Address
 from products.models import Product
-from .forms import UserProfileForm
+from .forms import UserProfileForm, AddressForm
 
 from checkout.models import Order
 
@@ -18,6 +18,8 @@ def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     wishlist = Wishlist.objects.filter(user=user)
     orders = profile.orders.all()
+    address = Address.objects.filter(user=user)
+    default_address = Address.objects.filter(user=request.user, set_as_default=True).first()
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
@@ -34,6 +36,8 @@ def profile(request):
         'form': form,
         'orders': orders,
         'wishlist': wishlist,
+        'address': address,
+        'default_address': default_address,
         'on_profile_page': True,
     }
 
@@ -102,3 +106,47 @@ def remove_from_wishlist(request, id):
         messages.error(request, 'Wishlist entry not found!')
         return HttpResponseRedirect(request.META.get("HTTP_REFERER", "/"))
 
+
+@login_required
+def add_address(request):
+    user_profile = request.user.userprofile
+    default_address = Address.objects.filter(user=request.user, set_as_default=True).first()
+
+    if request.method == 'POST':
+        address_form = AddressForm(request.POST)
+        if address_form.is_valid():
+            address = address_form.save(commit=False)
+            address.user = request.user
+
+            # If a new address is set as default
+            if address_form.cleaned_data.get('set_as_default', False):
+                # Unset the previous default address if it exists
+                if default_address:
+                    default_address.set_as_default = False
+                    default_address.save()
+                
+                address.set_as_default = True
+                # Update the user profile default address fields
+                user_profile.default_full_name = address.full_name
+                user_profile.default_phone_number = address.phone_number
+                user_profile.default_country = address.country
+                user_profile.default_postcode = address.postcode
+                user_profile.default_town_or_city = address.town_or_city
+                user_profile.default_street_address1 = address.street_address1
+                user_profile.default_street_address2 = address.street_address2
+                user_profile.default_county = address.county
+                user_profile.save()
+            address.save()
+            messages.success(request, 'Address added successfully')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Error adding address. Please check the form.')
+    else:
+        address_form = AddressForm()
+
+    context = {
+        'address_form': address_form,
+        'default_address': default_address
+    }
+
+    return render(request, 'user_profile/add_address.html', context)
