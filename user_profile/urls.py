f10726f3df42f3baae79bc645e9c4c0996dfd5fb
Denes Kalnoky <dkalnoky@gmail.com>
2024-06-30 20:38:50 +0000

    added add_address view to urlspatterns

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('order_history/<order_number>', views.order_history, name='order_history'),
     path('profile/wishlist/', views.wishlist, name='wishlist'),
     path('wishlist/add_to_wishlist/<int:id>', views.add_to_wishlist, name='add_to_wishlist'),
-    path('wishlist/remove_from_wishlist/<int:id>/', views.remove_from_wishlist, name='remove_from_wishlist')
+    path('wishlist/remove_from_wishlist/<int:id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+    path('address/add/', views.add_address, name='add_address')
     ]
