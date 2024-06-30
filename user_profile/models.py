c464b2712bc138e719708a588d932bb376ee7d19
Denes Kalnoky <dkalnoky@gmail.com>
2024-06-30 20:38:23 +0000

    added Address Model for Address CRUD functionality

--- user_profile/models.py
+++ user_profile/models.py
@@ -25,7 +25,6 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
@@ -39,19 +38,25 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         except UserProfile.DoesNotExist:
             UserProfile.objects.create(user=instance)
 
-#class Address(models.Model):
-#    user = models.OneToOneField(User, on_delete=models.CASCADE)
-#    default_full_name = models.CharField(max_length=50, null=False, blank=False, default='')
-#    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-#    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-#    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-#    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-#    default_county = models.CharField(max_length=80, null=True, blank=True)
-#    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-#    default_country = CountryField(blank_label='Country', null=True, blank=True)
-#
-#    def __str__(self):
-#        return f'{self.default_full_name}, {self.default_town_or_city}'
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=50, null=False, blank=False, default='')
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country', null=True, blank=True)
+    set_as_default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.default_full_name}, {self.default_town_or_city}'
+
+    #class Meta:
+    #    verbose_name_plural = 'Addresses'
+    #    unique_together = ('user', 'set_as_default')
+
 
 class Wishlist(models.Model):
     user = models.ForeignKey(
