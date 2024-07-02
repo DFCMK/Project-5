b624a3cd9c6e0097c4d1a09f71bd461e7b4565ae
Denes Kalnoky <dkalnoky@gmail.com>
2024-07-02 16:23:17 +0000

    added views for editing, deleting and adding addresses

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('profile/wishlist/', views.wishlist, name='wishlist'),
     path('wishlist/add_to_wishlist/<int:id>', views.add_to_wishlist, name='add_to_wishlist'),
     path('wishlist/remove_from_wishlist/<int:id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
-    path('address/add/', views.add_address, name='add_address')
+    path('address/add/', views.add_address, name='add_address'),
+    path('profile/address/<int:address_id>/edit/', views.edit_address, name='edit_address'),
+    path('profile/address/<int:address_id>/delete/', views.delete_address, name='delete_address'),
+    path('profile/address/<int:address_id>/set_default/', views.set_default_address, name='set_default_address'),
     ]
