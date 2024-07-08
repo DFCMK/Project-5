03ecbce73f645f64f2ad3b745579d09720bcf03b
Denes Kalnoky <dkalnoky@gmail.com>
2024-07-08 08:47:16 +0000

    added review filed to RatingForm to handel integer rating and text review

--- products/forms.py
+++ products/forms.py
@@ -3,14 +3,22 @@ from .models import Rating
 
 # inspired by Desphinx tutorial:
 # https://www.youtube.com/watch?v=TIDldj2BDuY
-class RateProductForm(forms.ModelForm):
+class RatingForm(forms.ModelForm):
     '''
     Rating Form, to rate Products
     '''
-    # rating = forms.ChoiceField(choices=[(1, '★'), (2, '★★'), (3, '★★★'), (4, '★★★★'), (5, '★★★★★')], widget=forms.RadioSelect)
-   
+    RATING_CHOICES = [
+        (1, '★'),
+        (2, '★★'),
+        (3, '★★★'),
+        (4, '★★★★'),
+        (5, '★★★★★'),
+    ]
+
+    rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.RadioSelect)
+    review = forms.CharField(widget=forms.Textarea, required=False)
+
     class Meta:
-        model =  Rating
-        fields = ['rating']
-        
+        model = Rating
+        fields = ['rating', 'review']
 
