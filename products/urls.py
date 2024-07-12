a3773db628dcf6a2e9740e5797eb2ff836fb3a9e
Denes Kalnoky <dkalnoky@gmail.com>
2024-07-12 17:31:24 +0000

    added edit_review view

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
     path('product_detail/rate/<int:product_id>/', views.rate_product, name='rate_product'),
+    path('product_detail/edit/<int:product_id>/<int:review_id>/', views.edit_review, name='edit_review'),
 ]
