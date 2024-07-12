6d494b55412f868b144b5ba5b3f2422cda3f05e6
Denes Kalnoky <dkalnoky@gmail.com>
2024-07-12 17:34:41 +0000

    deleted half_star filter, cause it's complexity

--- products/templatetags/custom_filters.py
+++ products/templatetags/custom_filters.py
@@ -7,18 +7,10 @@ register = template.Library()
 @register.filter(name='get_star_range')
 def get_star_range(value):
     try:
-        stars = float(value)
+        stars = Decimal(value)
         return range(int(stars))
     except (ValueError, TypeError):
         return range(0)
-
-@register.filter(name='half_star')
-def half_star(value):
-    try:
-        stars = Decimal(value)
-        return stars -int(stars) >= Deciaml('0.5')
-    except (ValueError, TypeError):
-        return False
 
 @register.filter(name='get_empty_stars')
 def get_empty_stars(value):
