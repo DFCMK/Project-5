a553e72632220e646af2c2ebda9de84f8cc2a12b
Denes Kalnoky <dkalnoky@gmail.com>
2024-07-31 22:53:23 +0000

    removed csp_report import

--- HandCraft/urls.py
+++ HandCraft/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import handler404, handler403, handler500, csp_report
+from .views import handler404, handler403, handler500
 from django.conf import settings
 from django.conf.urls.static import static
 
