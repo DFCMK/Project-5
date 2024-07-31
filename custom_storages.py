c801b0580d7f62b875a1e9ce9b9a5288c09bd041
Denes Kalnoky <dkalnoky@gmail.com>
2024-07-31 11:00:25 +0000

    out commented logging for boto3

--- custom_storages.py
+++ custom_storages.py
@@ -1,15 +1,15 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
-import logging
+#import logging
 
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
     default_acl = 'public-read'
 
-    def _save(self, name, content):
-        logger.debug(f'Saving file: {name}')
-        return super()._save(name, content)
+    #def _save(self, name, content):
+    #    logger.debug(f'Saving file: {name}')
+    #    return super()._save(name, content)
 
 
 class MediaStorage(S3Boto3Storage):
