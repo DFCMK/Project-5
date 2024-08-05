89bff5a029c0ab8167a648f503d01eda2fa812bb
Denes Kalnoky <dkalnoky@gmail.com>
2024-08-05 16:47:37 +0000

    addd wishlistcount to home

--- home/views.py
+++ home/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 
 def home(request):
     '''
-    Return to the Homepage
+    Return to the Homepage and updated wishlist count
     '''
-    return render(request, 'home/index.html')
+
+    if request.user.is_authenticated:
+        wishlist_count = request.user.wishlist_entries.count()
+    else:
+        wishlist_count = 0
+
+    context = {'wishlist_count': wishlist_count}
+       
+    return render(request, 'home/index.html', context)
