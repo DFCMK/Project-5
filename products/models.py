3978d6c65b549058fb5440a036795cf43a95d9fd
Denes Kalnoky <dkalnoky@gmail.com>
2024-08-10 20:53:46 +0000

    altered price field to ensure form negative integers wont be accepted

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from django.contrib.auth.models import User
 
 
@@ -24,7 +25,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
-    price = models.DecimalField(max_digits=7, decimal_places=2)
+    price = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0.01)])
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True, default=0)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
