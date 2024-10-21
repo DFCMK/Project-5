e6c1052c644ea39dcfa191492b8cdf14897255bd
DFCMK <dkalnoky@gmail.com>
2024-10-21 18:52:11 +0100

    deleted unused code snippets

--- custom_storages.py
+++ custom_storages.py
@@ -1,17 +1,10 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
-#import logging
 
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-    #default_acl = 'public-read'
-
-    #def _save(self, name, content):
-    #    logger.debug(f'Saving file: {name}')
-    #    return super()._save(name, content)
 
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
-    #default_acl = 'public-read'
